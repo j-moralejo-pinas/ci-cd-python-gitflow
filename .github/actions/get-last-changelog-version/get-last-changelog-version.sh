74c1ba010bbf747f47c6bef9207a46c9fe9cc671
Javier Moralejo Piñas <j.moralejo.pinas@gmail.com>
2025-09-23 12:08:03 +0200

    Updated workflows to be callable

--- .github/actions/get-last-changelog-version/get-last-changelog-version.sh
+++ .github/actions/get-last-changelog-version/get-last-changelog-version.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 # Arguments:
-# 1: Path to the changelog file (default: CHANGELOG.rst)
+# 1: Path to the changelog file (default: CHANGELOG.md)
 
-CHANGELOG_PATH="${1:-CHANGELOG.rst}"
+CHANGELOG_PATH="${1:-CHANGELOG.md}"
 
 if [[ -f "${CHANGELOG_PATH}" ]]; then
   LAST_TAG=$(awk '/^##[[:space:]]+v?[0-9]+\.[0-9]+\.[0-9]+[[:space:]]+-[[:space:]]+[0-9]{4}-[0-9]{2}-[0-9]{2}/{print $2; exit}' "${CHANGELOG_PATH}" || true)
