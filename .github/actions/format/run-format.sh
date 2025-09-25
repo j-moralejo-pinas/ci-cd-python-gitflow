d1de2d100993a2933c4339e57c9aa5ac71afc89f
Javier Moralejo Piñas <j.moralejo.pinas@gmail.com>
2025-09-25 15:44:18 +0200

    Modified docformatter

--- .github/actions/format/run-format.sh
+++ .github/actions/format/run-format.sh
@@ -21,7 +21,7 @@ pdm run ruff format .
 
 # Run docformatter: exit code 0 = ok, 3 = changes made; others should fail
 set +e
-pdm run docformatter --in-place --recursive src/
+pdm run docformatter src/
 code=$?
 set -e
 if [[ "$code" != "0" && "$code" != "3" ]]; then
